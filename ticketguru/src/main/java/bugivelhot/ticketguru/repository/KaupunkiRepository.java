0f649c487f16b5cc8345dbdf93256548cf37b394
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-19 20:58:28 +0300

    Poistettu kaupunki-entiteetti ja KaupunkiRepository
    Kaupunki-taulun tiedot siirretty osoitteet-tauluun, joten Kaupunki-entiteetti ja KaupunkiRepository ovat nyt turhia

--- ticketguru/src/main/java/bugivelhot/ticketguru/repository/KaupunkiRepository.java
+++
@@ -1,9 +0,0 @@
-package bugivelhot.ticketguru.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import bugivelhot.ticketguru.entity.Kaupunki;
-
-@Repository
-public interface KaupunkiRepository extends JpaRepository<Kaupunki, Integer> {}
