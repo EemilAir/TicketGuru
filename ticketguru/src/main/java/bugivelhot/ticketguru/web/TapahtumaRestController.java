2005c60b1fc1120d99393dcfce8901f30b03a606
c41t <eemi1.airaksinen@hotmail.com>
2024-09-30 13:52:07 +0300

    Tapahtuman DELETE metodin lisääminen

--- ticketguru/src/main/java/bugivelhot/ticketguru/web/TapahtumaRestController.java
+++ ticketguru/src/main/java/bugivelhot/ticketguru/web/TapahtumaRestController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,4 +32,9 @@ public class TapahtumaRestController {
         return tapahtumaRepository.findById(id);
     }
 
+    // DELETE: http://localhost:8080/api/tapahtumat/1
+    @DeleteMapping("{id}")
+    public void poistaTapahtuma(@PathVariable("id") Long id) {
+        tapahtumaRepository.deleteById(id);
+    }
 }
