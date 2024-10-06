814f834eb5b38417265b76f0ab464f09fa357cc0
miikka.vartiainen@gmail.com <miikka.vartiainen@gmail.com>
2024-10-06 13:52:52 +0300

    MyyntitapahtumaRestController-muutoksia

--- ticketguru/src/main/java/bugivelhot/ticketguru/web/MyyntitapahtumaRestController.java
+++ ticketguru/src/main/java/bugivelhot/ticketguru/web/MyyntitapahtumaRestController.java
@@ -1,9 +1,11 @@
 package bugivelhot.ticketguru.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import bugivelhot.ticketguru.model.Myyntitapahtuma;
@@ -29,26 +31,20 @@ public class MyyntitapahtumaRestController {
         return ResponseEntity.ok(myyntitapahtuma);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<Myyntitapahtuma> haeMyyntitapahtuma(@PathVariable("id") Long id) {
+        Optional<Myyntitapahtuma> myyntitapahtuma = myyntitapahtumaRepository.findById(id);
+        if (myyntitapahtuma.isPresent()) {
+            return ResponseEntity.ok(myyntitapahtuma.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
     @GetMapping
     public List<Myyntitapahtuma> haeKaikkiMyyntitapahtumat() {
         return myyntitapahtumaRepository.findAll();
     }
-
-
-/*         @GetMapping
-    public List<Tapahtuma> haeKaikkiTapahtumat(
-            @RequestParam(required = false) String nimi,
-            @RequestParam(required = false) String kategoria) {
-        if (nimi != null && kategoria != null) {
-            return tapahtumaRepository.findByNimiContainingIgnoreCaseAndKategoriaContainingIgnoreCase(nimi, kategoria);
-        } else if (nimi != null) {
-            return tapahtumaRepository.findByNimiContainingIgnoreCase(nimi);
-        } else if (kategoria != null) {
-            return tapahtumaRepository.findByKategoriaContainingIgnoreCase(kategoria);
-        }
-        return tapahtumaRepository.findAll();
-    } */
-
-
 
 }
